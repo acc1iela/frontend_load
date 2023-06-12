4e619fc42dd04ad2022f626673075cf2cb08c0e7
acc1iela <acc1iela@gmail.com>
2023-06-12 21:55:06 +0900

    update: 不要な処理を削除

--- typescript_load/extendsClass.ts
+++ typescript_load/extendsClass.ts
@@ -16,14 +16,3 @@ class Point2 {
 const point = new Point2();
 point.moveX(10);
 console.log(`x座標は${point.x}です`);
-
-class Point3D extends Point2 {
-  z: number;
-  constructor(x: number = 0, y: number = 0, z: number = 0) {
-    super(x, y);
-    this.z = z;
-  }
-  moveZ(n: number): void {
-    this.z += n;
-  }
-}
